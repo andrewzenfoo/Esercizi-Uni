dcdb055fc4520f57d23445ae4130553fa77cd59a
andrewzenfoo <andrewzen13@gmail.com>
2016-05-04 15:16:00 +0200

    Added javadoc

--- MyUtils/MyMath.java
+++ MyUtils/MyMath.java
@@ -2,6 +2,12 @@ import java.util.Random;
 
 public class MyMath {
 
+    /**
+     * This method calculates the maximum number
+     * @param num array of integers
+     * @return maximum integer
+     */
+
     public static Integer maxNumber(int[] num) {
         int max = 0;
         for (int i = 0; i < num.length; i++) {
@@ -12,6 +18,12 @@ public class MyMath {
         return max;
     }
 
+    /**
+     * This method calculates the average number in an array of doubles
+     * @param num array of doubles
+     * @return average number
+     */
+
     public static Double averageNumber(double[] num) {
         double average = 0;
         for (int i = 0; i < num.length; i++) {
@@ -21,6 +33,12 @@ public class MyMath {
         return average;
     }
 
+    /**
+     * This method calculates the average number in an array of ints
+     * @param num array of integers
+     * @return average number
+     */
+
     public static Double averageNumber(int[] num) {
         double average = 0;
         for (int i = 0; i < num.length; i++) {
@@ -29,6 +47,13 @@ public class MyMath {
         average = average / num.length;
         return average;
     }
+
+    /**
+     * This method computes a random int
+     * @param min minimum number (inclusive)
+     * @param max maximum number (inclusive)
+     * @return random number between min and max parameters
+     */
 
     public static int randomInt(int min, int max) {
         Random foo = new Random();
